683daa5448453ed9065a116cfb12f9ce03df31eb
sani4618 <sanijoseph2018@gmail.com>
2023-09-26 21:34:47 +0530

    frontend completed

---
+++ src/components/Viewall.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigationbar from './Navigationbar'
+
+const Viewall = () => {
+  return (
+    <div>
+        <Navigationbar/>
+    </div>
+  )
+}
+
+export default Viewall
